2d2ca44405cb6348e33c9a389ff9418ae9d3557a
Ivan Vlahov <ivan.vlahov@gmail.com>
2021-01-26 04:23:22 +0100

    edited listen

--- bot.js
+++ bot.js
@@ -19,4 +19,4 @@ http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write('Hello World!');
     res.end();
-}).listen(8080);
+}).listen(process.env.PORT || 8080);
