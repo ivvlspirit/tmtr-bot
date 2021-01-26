8174b957a9ae379c024aa2ec7f61d2ea1efbab2f
Ivan Vlahov <ivan.vlahov@gmail.com>
2021-01-26 23:05:48 +0100

    edited dependencies and removed web-related stuff

--- bot.js
+++ bot.js
@@ -1,9 +1,5 @@
 require('dotenv').config();
 
-const http = require('http');
-const ping = require('ping');
-
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -14,18 +10,3 @@ client.login(process.env.TOKEN);
 client.on('ready', () => console.log('The bot is ready.'));
 
 client.on('message', commandHandler);
-
-const intervalTime = 1000*60*20;
-
-setInterval(() => {
-    ping.sys.probe('tmtr-bot.herokuapp.com', function(isAlive){
-        console.log('Server status: ' + (isAlive ? 'running' : 'sleeping'));
-    });
-}, intervalTime);
-
-
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello World!');
-    res.end();
-}).listen(process.env.PORT || 8080);
