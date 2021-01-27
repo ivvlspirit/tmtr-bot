1a9d347937111e119b6dcb555323d8fdebdad4e6
Ivan Vlahov <ivan.vlahov@gmail.com>
2021-01-27 23:58:15 +0100

    gif command fix

--- commands/gif.js
+++ commands/gif.js
@@ -12,17 +12,22 @@ module.exports = async function(msg, args){
     let response = await fetch(url);
     let json = await response.json();
 
-    var randomGif = Math.floor(Math.random() * json.results.length);
+    try{
+        var randomGif = Math.floor(Math.random() * json.results.length);
 
-    var gifUrl = json.results[randomGif].media[0].mediumgif.url;
-    var gifFilename = gifUrl.split('/')[gifUrl.split('/').length-1];
+        var gifUrl = json.results[randomGif].media[0].mediumgif.url;
+        var gifFilename = gifUrl.split('/')[gifUrl.split('/').length-1];
 
-    var message = new Discord.MessageEmbed()
-        .setTitle('Tenor GIF Keyboard')
-        .setURL('https://tenor.com')
-        .setColor('#fc038c')
-        .attachFiles([gifUrl])
-        .setImage(`attachment://${gifFilename}`);
+        var message = new Discord.MessageEmbed()
+            .setTitle('Tenor GIF Keyboard')
+            .setURL('https://tenor.com')
+            .setColor('#fc038c')
+            .attachFiles([gifUrl])
+            .setImage(`attachment://${gifFilename}`);
 
-    msg.channel.send(message);
+        msg.channel.send(message);
+    }catch(err){
+        console.log(err);
+        msg.channel.send(gifQuery + ' is a dumb query.');
+    }
 }
